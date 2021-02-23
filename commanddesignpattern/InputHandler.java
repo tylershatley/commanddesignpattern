56aede43f9663a78d6ef0b2da649d70d652a669d
tylershatley <shatleymtyler@gmail.com>
2021-02-23 11:47:04 -0500

    i dont even know anymore

--- commanddesignpattern/InputHandler.java
+++ commanddesignpattern/InputHandler.java
@@ -1,13 +1,20 @@
 package commanddesignpattern;
 import java.util.HashMap;
-import java.util.Map;
 
 public class InputHandler {
     //idfk what to do
+    private Robot robot;
+    private PickupCommand puc;
     private HashMap <String, Command> commands;
 
-    public InputHandler() {
+    public InputHandler(Robot robot) {
         commands = new HashMap<String, Command>();
+        commands.put("pickup", puc);
     }
 
+    public void inputEntered(String data) {
+        if(commands.containsKey(data)) {
+            commands.get(data);
+        }
+    }
 }
