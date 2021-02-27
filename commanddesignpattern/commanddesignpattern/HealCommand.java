73bd18af4ffa074dfa203b5f2190397a810abf41
Reece Peters <ppeters@email.sc.edu>
2021-02-27 18:43:02 -0500

    Javadoc in HealCommand

--- commanddesignpattern/commanddesignpattern/HealCommand.java
+++ commanddesignpattern/commanddesignpattern/HealCommand.java
@@ -1,18 +1,24 @@
 package commanddesignpattern;
 
 public class HealCommand implements Command{
-    private Robot robot;
     /**
-     * initializes heal command robot
+     * This class is used to create an instance of a command healing
+     * inherits from class VehicleDecorator
+     */
+
+    private Robot robot;
+
+    /**
+     * This method is a constructor which initializes heal command robot
      * @param robot
      */
     public HealCommand(Robot robot)
     {
         this.robot = robot;
     }
+
     /**
-     * implemented heal command
-     * calls robot void heal function
+     * This method calls heal function in robot and returns nothing
      */
     public void execute()
     {
