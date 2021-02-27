a343eafb8db5a788823535bfeca5b23135b1a3f7
tylershatley <shatleymtyler@gmail.com>
2021-02-27 16:53:44 -0500

    javadocd

--- commanddesignpattern/commanddesignpattern/JumpCommand.java
+++ commanddesignpattern/commanddesignpattern/JumpCommand.java
@@ -2,10 +2,17 @@ package commanddesignpattern;
 public class JumpCommand implements Command {
     private Robot robot;
 
+    /**
+     * initializes jump command robot
+     * @param robot
+     */
     public JumpCommand(Robot robot) {
         this.robot = robot;
     }
-
+    /**
+     * implemented jump command
+     * calls robot void jump function
+     */
     @Override
     public void execute() {
         robot.jump();
